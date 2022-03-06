20be503738fb9ff443a5b17e207466a36a493065
dcampbellcreative <dougcampbell1990@gmail.com>
2022-03-06 12:44:16 -0500

    add assests

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar/NavBar';
+import { NavBarSpacer } from './components/NavBarSpacer/NavBarSpacer'
 import { Home } from './components/Home/Home';
 import { AboutMe } from './components/AboutMe/AboutMe'
 import { MyWork } from './components/MyWork/MyWork';
-import { Resume } from './components/Resume/Resume';
 import { Contact } from './components/Contact/Contact'
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <NavBar />
         <Routes>
-          <Route exact path='/' element={<MyWork />} />
+          <Route exact path='/' element={<Home />} />
           <Route exact path='/home' element={<Home />} />
           <Route exact path='/aboutme' element={<AboutMe />} />
-          <Route exact path='/resume' element={<Resume />} />
+          <Route exact path='/projects' element={<MyWork />} />
           <Route exact path='/contact' element={<Contact />} />
-        </Routes>
-      </Router>
 
+        </Routes>
+      </Router> */}
+      {/* <Route exact path='/' element={<Home />} /> */}
+      <NavBar />
+      {/* <NavBarSpacer /> */}
+      <Home />
+      <AboutMe />
+      <MyWork />
+      <Contact />
     </div>
   );
 }
